a8c9a0e69d63727e076e8ebb08c61cec8f25335a
tomasdelmastro <tomasdelmastro@gmail.com>
2022-06-05 18:47:27 -0300

    App de Gastos
    App de gastos - arreglar particle

---
+++ main.js
@@ -0,0 +1,40 @@
+const nombre = document.getElementById('nombre');
+const monto = document.getElementById('monto');
+const total = document.getElementById('total');
+const lista = document.getElementById('lista');
+const set = new Set();
+
+let people = 0;
+let sum = 0;
+
+const sumo = () => {
+	sum += parseInt(monto.value);
+}
+
+function esta() {
+	const esta = !set.has(nombre.value);
+	return esta
+}
+
+function act(){
+	total.innerHTML = `Total: ${sum} y cada uno debe pagar ${sum/people}`;
+}
+
+const addPeople = () => {
+	people++;
+	set.add(nombre.value);
+	sumo()
+	act()
+}
+
+const myFunction=() => {
+	if(esta()){
+		addPeople();
+		act();
+		lista.innerHTML += `<p>${nombre.value}</p>`;
+	}
+	else{
+		sumo()
+		act()
+	}
+}
